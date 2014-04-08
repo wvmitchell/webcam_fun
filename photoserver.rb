0714f996ed507b58dda7b959df53224709a01360
Will Mitchell <wvmitchell@gmail.com>
2014-04-08 11:52:03 -0600

    basic photoserver class implemented

--- photoserver.rb
+++ photoserver.rb
@@ -1,2 +1,21 @@
 require 'av_capture'
 require 'drb'
+
+class PhotoServer
+
+  attr_reader :photo_request, :photo_response
+
+  def initialize
+    @photo_request = Queue.new
+    @photo_response = Queue.new
+    @mutex = Mutex.new
+  end
+
+  def take_photo
+    @mutex.synchronize do
+      photo_request << 'x'
+      photo_response.pop
+    end
+  end
+
+end
